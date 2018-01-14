684e7ccb17f8bf5910271a762fdaf96717ef272d
Daniel Ruatta <druatta@ucsc.edu>
2018-01-13 23:51:40 -0800

    Homework 1
    Unit tests created, now I need to get them to pass

---
+++ Homework/Homework 1/HelloWorldWithArguments/HelloWorldWithArguments/ReversingArguments.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ReversingArguments
+{
+    public class ReversingArgumentsMethods
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        public static void GetASingleUserInput()
+        {
+
+        }
+
+        static void PrintMyOriginalArguments(string[] args)
+        {
+            Console.WriteLine("You entered the following arguments:");
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("{0}", args[i]);
+            }
+        }
+
+        static void PrintMyReversedArguments()
+        {
+
+        }
+
+        static void ReverseMyArguments()
+        {
+
+        }
+
+        static void WaitForUserInput()
+        {
+            Console.WriteLine("\nPress any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
