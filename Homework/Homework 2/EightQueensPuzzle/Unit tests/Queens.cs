e9bde5a620004a3a3f3819e0bff313e1cf41e8ba
Daniel Ruatta <druatta@ucsc.edu>
2018-01-17 07:07:59 -0800

    Homework 2
    Outlining

---
+++ Homework/Homework 2/EightQueensPuzzle/Unit tests/Queens.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unit_tests
+{
+    [TestClass]
+    public class Queens
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
